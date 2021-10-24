bee236256f1388a181ccda8a2b360a4c06d35eca
ton1czech <tonyasek007@gmail.com>
2021-10-24 23:02:19 +0200

    🎁NEW: getting familiar with chakra ui

---
+++ constants/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  test1: '#fff',
+  test2: '#313131',
+}
+
+const theme = extendTheme({ colors })
+
+export default theme
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../constants/theme'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
-
-export default MyApp
