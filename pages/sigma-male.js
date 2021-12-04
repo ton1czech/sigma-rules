0067aca51b41737fe6e1539cbf9c05ab7b92fbb7
ton1czech <tonyasek007@gmail.com>
2021-12-05 00:12:57 +0100

    👌IMPROVE: apostrofs

--- pages/sigma-male.js
+++ pages/sigma-male.js
@@ -32,14 +32,14 @@ export default function SigmaMale() {
           ambition and the popularity but without the huge ego, aggression, and need for validation from others. He can
           socialise just fine and can manipulate and adapt to situations as and when needed. Even taking on leadership
           roles if he wants to but he has no interest in the opinions of others and is more than happy to be on his own.
-          He isn't afraid to play by his own rules, and he comes across as both exciting and likeable. He is calculated
-          and cunning, taking his time to listen to others and respond carefully. He lives his life with intention - a
-          thirst for knowledge and gaining new skills, as well as seeking out new experiences and adventure. People find
-          him to be intelligent and funny, with a quick wit and a great sense of humour. He doesn't need a partner in
-          his life, but his mystery and willingess to break the rules attracts the females in droves. The Sigma Male is
-          independent thinker, and lone wolf rather than being in a 'pack' like an alpha. He doesn't seek to lead nor
-          follow but is comfortable in either position if he must be. He's usually a good judge of character, abstaining
-          from immediate judgment of others.
+          He isn&apos;t afraid to play by his own rules, and he comes across as both exciting and likeable. He is
+          calculated and cunning, taking his time to listen to others and respond carefully. He lives his life with
+          intention - a thirst for knowledge and gaining new skills, as well as seeking out new experiences and
+          adventure. People find him to be intelligent and funny, with a quick wit and a great sense of humour. He
+          doesn&apos;t need a partner in his life, but his mystery and willingess to break the rules attracts the
+          females in droves. The Sigma Male is independent thinker, and lone wolf rather than being in a
+          &apos;pack&apos; like an alpha. He doesn&apos;t seek to lead nor follow but is comfortable in either position
+          if he must be. He&apos;s usually a good judge of character, abstaining from immediate judgment of others.
         </Text>
         <Heading {...headingProps}>Pros</Heading>
         <Text {...textProps}>
@@ -51,11 +51,11 @@ export default function SigmaMale() {
           to walk away.
         </Text>
         <Text {...textProps}>
-          - Efficiency = The Sigma Male isn't bound to the usual trappings of cultural norms and societal pressures,
-          which makes him live so much more efficiently. He doesn't feel the need to waste time and money on anything
-          that doesn't serve his own interests, personal goals or plans. Not caring about the judgments and opinions of
-          others also allows him to be 100% genuine and authentic, as he sees no need in trying to win the approval or
-          validation of anyone except himself
+          - Efficiency = The Sigma Male isn&apos;t bound to the usual trappings of cultural norms and societal
+          pressures, which makes him live so much more efficiently. He doesn&apos;t feel the need to waste time and
+          money on anything that doesn&apos;t serve his own interests, personal goals or plans. Not caring about the
+          judgments and opinions of others also allows him to be 100% genuine and authentic, as he sees no need in
+          trying to win the approval or validation of anyone except himself
         </Text>
         <Heading {...headingProps}>Cons</Heading>
         <Text {...textProps}>
